e5afa76a07751f8f4ce421b5312c03f4843efb72
murray-pocha <murray.pocha@gmail.com>
2024-12-28 12:40:56 -0700

    adjust to add not null statements in missing places, add delete cascade in missing owner_id column

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -6,33 +6,36 @@ DROP TABLE IF EXISTS users CASCADE;
 
 -- create tables
 CREATE TABLE users (
-  id SERIAL PRIMARY KEY,
+  id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
   email VARCHAR(255) UNIQUE NOT NULL,
   password VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE properties (
-  id SERIAL PRIMARY KEY,
-  owner_id INTEGER NOT NULL REFERENCES users(id),
+  id SERIAL PRIMARY KEY NOT NULL,
+  owner_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+
   title VARCHAR(255) NOT NULL,
   description TEXT,
-  thumbnail_photo_url VARCHAR(255),
-  cover_photo_url VARCHAR(255),
-  cost_per_night INTEGER NOT NULL,
-  parking_spaces INTEGER DEFAULT 0,
-  number_of_bathrooms INTEGER DEFAULT 0,
-  number_of_bedrooms INTEGER DEFAULT 0,
-  country VARCHAR(255),
-  street VARCHAR(255),
-  city VARCHAR(255),
-  province VARCHAR(255),
-  post_code VARCHAR(255),
-  active BOOLEAN DEFAULT TRUE
+  thumbnail_photo_url VARCHAR(255) NOT NULL,
+  cover_photo_url VARCHAR(255) NOT NULL,
+  cost_per_night INTEGER NOT NULL DEFAULT 0,
+  parking_spaces INTEGER NOT NULL DEFAULT 0,
+  number_of_bathrooms INTEGER NOT NULL DEFAULT 0,
+  number_of_bedrooms INTEGER NOT NULL DEFAULT 0,
+
+  country VARCHAR(255) NOT NULL,
+  street VARCHAR(255) NOT NULL,
+  city VARCHAR(255) NOT NULL,
+  province VARCHAR(255) NOT NULL,
+  post_code VARCHAR(255) NOT NULL,
+
+  active BOOLEAN NOT NULL DEFAULT TRUE
 );
 
 CREATE TABLE reservations (
-  id SERIAL PRIMARY KEY,
+  id SERIAL PRIMARY KEY NOT NULL,
   start_date DATE NOT NULL,
   end_date DATE NOT NULL,
   property_id INTEGER NOT NULL REFERENCES properties(id) ON DELETE CASCADE,
@@ -40,11 +43,11 @@ CREATE TABLE reservations (
 );
 
 CREATE TABLE property_reviews (
-  id SERIAL PRIMARY KEY,
+  id SERIAL PRIMARY KEY NOT NULL,
   guest_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
   property_id INTEGER NOT NULL REFERENCES properties(id) ON DELETE CASCADE,
   reservation_id INTEGER NOT NULL REFERENCES reservations(id) ON DELETE CASCADE,
-  rating SMALLINT CHECK (rating >= 1 AND rating <= 5),
+  rating SMALLINT NOT NULL DEFAULT 0,
   message TEXT
 );
 
